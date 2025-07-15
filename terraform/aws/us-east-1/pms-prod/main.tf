e85e59dd960130bc2a44cc6f63354dfc560ab0c6
HyeongSeok Kim <tiryul@gmail.com>
2025-07-15 15:36:22 +0900

    feat: Add Kuberay and Karpenter configurations for EKS deployment
    - Created a new Kubernetes Service for Ray with NLB configuration in `ncp/kr/ray-nlb.yaml`.
    - Added Terraform lock file for provider dependencies in `terraform/aws/us-east-1/pms-prod/.terraform.lock.hcl`.
    - Introduced AWS ECR public authorization token data source in `terraform/aws/us-east-1/pms-prod/datas.tf`.
    - Defined Karpenter EC2NodeClass for CPU nodes in `karpenter-manifests/cpu/node.class.yaml`.
    - Created Karpenter NodePool for CPU nodes in `karpenter-manifests/cpu/node.pool.yaml`.
    - Added default Karpenter EC2NodeClass in `karpenter-manifests/default/node.class.yaml`.
    - Created default Karpenter NodePool in `karpenter-manifests/default/node.pool.yaml`.
    - Defined Karpenter EC2NodeClass for GPU nodes in `karpenter-manifests/gpu/node.class.yaml`.
    - Created Karpenter NodePool for GPU nodes in `karpenter-manifests/gpu/node.pool.yaml`.
    - Added NVIDIA device plugin DaemonSet manifest in `kubectl-manifests/nvidia-device-plugin.yaml`.
    - Implemented main Terraform configuration for EKS, Kuberay, and Karpenter in `main.tf`.
    - Defined outputs for EKS cluster in `outputs.tf`.
    - Configured AWS and Kubernetes providers in `providers.tf`.
    - Created Ray cluster configuration in `ray.tf`.
    - Added variables for cluster configuration in `variables.tf`.
    - Specified required Terraform and provider versions in `versions.tf`.

---
+++ terraform/aws/us-east-1/pms-prod/main.tf
@@ -0,0 +1,56 @@
+module "vpc" {
+  source = "../../../modules/vpc"
+  cluster_name    = var.cluster_name
+  number_of_azs = 3
+  tags = var.tags
+  
+}
+
+module "eks" {
+  source  = "../../../modules/eks"
+  cluster_name    = var.cluster_name
+  cluster_version = var.k8s_version
+  tags = var.tags
+  vpc_id = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+  intra_subnets = module.vpc.intra_subnets
+  depends_on = [ module.vpc ]
+}
+
+module "kubectl" {
+  source = "../../../modules/kubectl"
+  template_env = {
+    "cluster_name": var.cluster_name,
+    "node_iam_role_name": module.karpenter.node_iam_role_name,
+  }
+  depends_on = [ module.eks ]
+  manifests_path = "${path.module}/kubectl-manifests"
+}
+
+module "kuberay" {
+  source = "../../../modules/kuberay"
+  namespace = "ray"
+  depends_on = [ module.eks ]
+}
+
+module "karpenter" {
+  source = "../../../modules/karpenter"
+  cluster_name = var.cluster_name
+  cluster_endpoint = module.eks.cluster_endpoint
+  cluster_queue_name = var.cluster_name
+  aws_token_username = data.aws_ecrpublic_authorization_token.token.user_name
+  aws_token_password = data.aws_ecrpublic_authorization_token.token.password
+  cluster_certificate_authority_data = module.eks.cluster_certificate_authority_data
+  manifests_path = "${path.module}/karpenter-manifests"
+  tags = var.tags
+  depends_on = [ module.eks ]
+}
+
+module "aws-load-balancer-controller" {
+  source = "../../../modules/aws-load-balancer-controller"
+  region = var.region
+  cluster_name = var.cluster_name
+  vpc_id = module.vpc.vpc_id
+  depends_on = [ module.eks ]
+  cluster_oidc_issuer_url = module.eks.cluster_oidc_issuer_url
+}
---
+++ terraform/aws/us-east-1/pms-prod/ray.tf
@@ -0,0 +1,36 @@
+locals {
+  ray_cluster_name = "prod-ray-cluster"
+  namespace = "ray"
+}
+
+module "ray-cluster" {
+  depends_on = [ module.eks, module.kuberay, module.karpenter ]
+  source = "../../../modules/ray-cluster"
+
+  ray_cluster_name = local.ray_cluster_name
+  namespace = local.namespace
+  ray_version = "2.40.0"
+  ray_runtime_cpu = "ghcr.io/4by4inc-pixell/pms-ray-cluster:1.0.1-basic"
+  ray_runtime_gpu = "ghcr.io/4by4inc-pixell/pms-ray-cluster:1.0.1-trt"
+  ray_head_cpu_use = "8"
+  ray_head_ram_use = "16Gi"
+  ray_cpuworker_cpu_use = "1"
+  ray_cpuworker_ram_use = "1Gi"
+  ray_cpuworker_replica_min = 0
+  ray_cpuworker_replica_max = 0
+  ray_gpuworker_cpu_use = "190000m"
+  ray_gpuworker_ram_use = "190Gi"
+  ray_gpuworker_gpu_use = "8"
+  ray_gpuworker_replica_min = 0
+  ray_gpuworker_replica_max = 20
+}
+
+module "ray-cluster-nlb" {
+  depends_on = [ module.eks, module.kuberay, module.karpenter ]
+  source = "../../../modules/ray-cluster-nlb"
+ 
+  cluster_name = var.cluster_name
+  vpc_id = module.vpc.vpc_id
+  namespace = local.namespace
+  ray_cluster_name = local.ray_cluster_name
+}
